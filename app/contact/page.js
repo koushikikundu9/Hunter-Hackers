3a44aab9aee4641988e55e5d49785507a9363cc6
Koushiki Kundu <kundukoushiki73@gmail.com>
2025-06-29 20:08:33 +0530

    contacts updated

--- app/contact/page.js
+++ app/contact/page.js
@@ -19,7 +19,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
   e.preventDefault();
   console.log('Form Submitted:', formData);
-
+  alert("Form recorded");
   try {
     const response = await fetch("http://localhost:3000/api/postcontact", {
       method: "POST",
